8e1d53b4dcf0fc38de28bea65e0460a791179677
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-04 11:27:48 -0300

    style: statusbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native'
 import Home from './screens/home';
 
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
+       <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Home />
     </>
   );
